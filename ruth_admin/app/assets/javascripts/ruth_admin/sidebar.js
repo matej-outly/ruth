e1e4298da608ae1e455de10219e10f2001f4a7c7
Matěj Outlý <matej.outly@clockstar.cz>
2017-02-17 10:09:22 +0100

    RuthAdmin - Container width fixed

--- ruth_admin/app/assets/javascripts/ruth_admin/sidebar.js
+++ ruth_admin/app/assets/javascripts/ruth_admin/sidebar.js
@@ -1,35 +1,67 @@
-function sidebar_ready()
+function RuthSidebar(options)
 {
-	$(".sidebar-toggle").click(function(e) {
-		e.preventDefault();
-		
-		// Toggle class in DOM
-		$("body").toggleClass("sidebar-active");
+	this.storageKey = 'ruth_sidebar';
+	this.options = (typeof options !== 'undefined' ? options : {});
+}
+RuthSidebar.prototype = {
+	constructor: RuthSidebar,
+	saveScroll: function()
+	{
+		var scroll = $(".sidebar-content").scrollTop();
+		localStorage.setItem(this.storageKey + '_scroll', scroll);
+	},
+	loadScroll: function()
+	{
+		var scroll = localStorage.getItem(this.storageKey + '_scroll');
+		$(".sidebar-content").scrollTop(scroll);
+	},
+	reload: function()
+	{
+		this.loadScroll();
+	},
+	ready: function() 
+	{
+		var _this = this;
 
-		// Is sidebar active?
-		var sidebarActive = $("body").hasClass("sidebar-active");
-		
-		// Toggle flag in local storage
-		localStorage.setItem('ruth_sidebar_active', sidebarActive);
-	});
+		// Active toggle
+		$(".sidebar-toggle").click(function(e) {
+			e.preventDefault();
+			
+			// Toggle class in DOM
+			$("body").toggleClass("sidebar-active");
 
-	// Load current state from storage
-	var sidebarActiveByDefault = localStorage.getItem('ruth_sidebar_active');
-	if (sidebarActiveByDefault == "false") {
-		$("body").removeClass("sidebar-active");
-	} else {
-		$("body").addClass("sidebar-active");
+			// Is sidebar active?
+			var sidebarActive = $("body").hasClass("sidebar-active");
+			
+			// Toggle flag in local storage
+			localStorage.setItem(_this.storageKey + '_active', sidebarActive);
+		});
+
+		// Load current active state from storage
+		var sidebarActiveByDefault = localStorage.getItem(_this.storageKey + '_active');
+		if (sidebarActiveByDefault == "false") {
+			$("body").removeClass("sidebar-active");
+		} else {
+			$("body").addClass("sidebar-active");
+		}
+
+		// After some time set animated classes
+		setTimeout(function() {
+			$(".sidebar-right").addClass("animated");
+			$(".sidebar-left").addClass("animated");
+			$(".sidebar-container").addClass("animated");
+		}, 500);
+
+		// Scrollbar
+		$(".sidebar-content").perfectScrollbar();
+
+		// Save scroll position to local storage
+		$(".sidebar-content").scroll(this.saveScroll.bind(this));
 	}
-
-	// Scrollbar
-	$(".sidebar-content").perfectScrollbar();
-
-	// After some time set animated classes
-	setTimeout(function() {
-		$(".sidebar-right").addClass("animated");
-		$(".sidebar-left").addClass("animated");
-		$(".sidebar-container").addClass("animated");
-	}, 500);
 }
 
-$(document).ready(sidebar_ready);
+var ruth_sidebar = null;
+$(document).ready(function() {
+	ruth_sidebar = new RuthSidebar({});
+	ruth_sidebar.ready();
+});
