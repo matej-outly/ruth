9c53c09a0f5137ca5aa5c84984c807d30ca90c2a
Jaroslav Mlejnek <jar.gml55@gmail.com>
2017-04-14 11:13:46 +0200

    Ajax Form now supports invisible recaptcha. It is not neccessary to make some special options, AjaxForm will detecct invisible recaptcha automatically.

--- ruth_base/app/assets/javascripts/ajax_form/ajax_form.js
+++ ruth_base/app/assets/javascripts/ajax_form/ajax_form.js
@@ -12,23 +12,26 @@
 /*****************************************************************************/
 /*
 /* Options:
-/* - flashSelector (string) .... Selector for inline flash container, if not 
+/* - flashSelector (string) .... Selector for inline flash container, if not
 /*                               provided, Alertify plugin used
 /* - successMessage (string) ... Message displayed at success
 /* - errorMessage (string) ..... Message displayed at error
 /* - clearOnSubmit (boolean) ... Clear form values when successfuly submitted?
-/* - behaviorOnSubmit (none|hide|redirect) ... What to do when form is 
+/* - behaviorOnSubmit (none|hide|redirect) ... What to do when form is
 /*                               successfuly submitted
-/* - hideTimeout (integer) ..... How many seconds to wait until hidden form is 
-/*                               shown again, hide forever if null (only for  
+/* - hideTimeout (integer) ..... How many seconds to wait until hidden form is
+/*                               shown again, hide forever if null (only for
 /*                               'hide' behavior)
-/* - copyToObject (string) ..... JS object implementing addItem() and 
-/*                               changeItem()  functions where submitted data 
+/* - copyToObject (string) ..... JS object implementing addItem() and
+/*                               changeItem()  functions where submitted data
 /*                               will be copied
-/* - redirectUrl (string) ...... URL where to redirect when form is successfuly 
+/* - redirectUrl (string) ...... URL where to redirect when form is successfuly
 /*                               submitted (necessary for 'hide' behavior)
-/* - showUrl (string) .......... URL where edited object can be loaded through 
+/* - showUrl (string) .......... URL where edited object can be loaded through
 /*                               AJAX (necessary if copyToObject defined)
+/* - invisibleRecaptcha (boolean) ...  Use invisible recaptcha? Default - autodetect
+/* - logCallback (boolean) ..... Log success and error callbacks into console
+/*                               (for debugging purposes)
 /*
 /* Coresponding options set via data attribute:
 /* - data-af-flash-selector
@@ -40,6 +43,8 @@
 /* - data-af-copy-to-object
 /* - data-af-redirect-url
 /* - data-af-show-url
+/* - data-af-invisible-recaptcha
+/* - data-af-log-callback
 /*
 /*****************************************************************************/
 
@@ -58,11 +63,14 @@
 			copyToObject: null,
 			redirectUrl: null,
 			showUrl: null,
+			invisibleRecaptcha: null, // null means auto-detect
+			logCallback: false,
 		}, setOptions);
 
 		// Constructor
-		function AjaxForm(form, options) 
+		function AjaxForm(form, options)
 		{
+			// Basic configuration
 			this.$form = $(form);
 			this.options = (typeof options !== 'undefined' ? options : {});
 
@@ -77,36 +85,48 @@
 				copyToObject: this.$form.attr("data-af-copy-to-object"),
 				redirectUrl: this.$form.attr("data-af-redirect-url"),
 				showUrl: this.$form.attr("data-af-show-url"),
+				invisibleRecaptcha: (this.$form.attr("data-af-invisible-recaptcha") ? (this.$form.attr("data-af-invisible-recaptcha") == "true") : undefined),
+				logCallback: (this.$form.attr("data-af-log-callback") ? (this.$form.attr("data-af-log-callback") == "true") : undefined),
 			};
 			for (var key in this.options) {
 				if (typeof overrideOptions[key] !== 'undefined') {
 					this.options[key] = overrideOptions[key];
 				}
 			}
+
+			// Advanced configuration
+			this.$submitButton = this.$form.find("[type='submit']");
+			this.$recaptchaSubmitButton = this.$form.find("button.g-recaptcha");
+			this.invisibleRecaptchaId = "onInvisibleRecaptchaFormSubmit_" + this.id();
+
+			if (options.invisibleRecaptcha === null) {
+				// Autodetect invisible recaptcha by recaptcha id
+				options.invisibleRecaptcha = (this.$recaptchaSubmitButton.length !== 0);
+			}
 		}
 
 		// Form URL
-		AjaxForm.prototype.url = function() 
+		AjaxForm.prototype.url = function()
 		{
 			return this.$form.attr("action");
 		}
 
 		// Form ID
-		AjaxForm.prototype.id = function() 
+		AjaxForm.prototype.id = function()
 		{
 			return this.$form.attr("id");
 		}
 
 		// Set flash message on success or error
-		AjaxForm.prototype.setFlashMessage = function(result) 
+		AjaxForm.prototype.setFlashMessage = function(result)
 		{
-			
+
 			// Set message to flash selector
 			if (this.options.flashSelector) {
 				var $flash = $(this.options.flashSelector);
 				var message = result ? this.options.successMessage : this.options.errorMessage;
 				$flash.val(message);
-			
+
 			// Set message to alertify
 			} else if (typeof alertify != 'undefined') {
 				if (result) {
@@ -117,7 +137,7 @@
 			}
 		}
 
-		AjaxForm.prototype.behaveOnSubmit = function() 
+		AjaxForm.prototype.behaveOnSubmit = function()
 		{
 			var self = this;
 
@@ -139,14 +159,14 @@
 			} else if (this.options.behaviorOnSubmit == "redirect") {
 				self.clearForm();
 				// TODO
-			
+
 			// Nothing
 			} else {
 				self.clearForm();
 			}
 		}
 
-		AjaxForm.prototype.copyToObject = function(id) 
+		AjaxForm.prototype.copyToObject = function(id)
 		{
 			var self = this;
 
@@ -230,34 +250,64 @@
 			this.clearRecaptcha();
 		}
 
+		AjaxForm.prototype.submitForm = function()
+		{
+			// URL
+			var url = this.url();
+
+			this.$form.addClass("af-sending-request");
+			this.$submitButton.prop("disabled", true);
+
+			// Request
+			var self = this;
+			$.ajax({
+				url: url,
+				dataType: "json",
+				type: "POST",
+				data: this.$form.serialize(),
+
+				// Success data fetch
+				success: function(callback) {
+					if (options.logCallback) {
+						console.log(callback);
+					}
+
+					self.$form.removeClass("af-sending-request");
+					self.$submitButton.prop("disabled", false);
+					self.ajaxSuccess(callback);
+				},
+
+				// Error data fetch
+				error: function(callback) {
+					if (options.logCallback) {
+						console.log(callback);
+					}
+
+					self.$form.removeClass("af-sending-request");
+					self.$submitButton.prop("disabled", false);
+					self.ajaxError(callback);
+				},
+			});
+		}
+
 		AjaxForm.prototype.activateAjax = function()
 		{
-			var self = this;
-			var $submitButton = this.$form.find("[type='submit']");
+			if (options.invisibleRecaptcha === true) {
+				// Submiting form in invisible recaptcha callback,
+				// callback must be within window "namespace"
+				window[this.invisibleRecaptchaId] = this.submitForm.bind(this);
 
-			$submitButton.click(function(e) {
-				e.preventDefault();
-
-				// URL
-				var url = self.url();
-
-				self.$form.addClass("af-sending-request");
-				$submitButton.prop("disabled", true);
-
-				// Request
-				$.ajax({
-					url: url,
-					dataType: "json",
-					type: "POST",
-					data: self.$form.serialize(),
-
-					// Success data fetch
-					success: function(callback) { self.$form.removeClass("af-sending-request"); $submitButton.prop("disabled", false); self.ajaxSuccess(callback); },
-
-					// Error data fetch
-					error: function(callback) { self.$form.removeClass("af-sending-request"); $submitButton.prop("disabled", false); self.ajaxError(callback); },
+				// Add recaptcha callback to button
+				this.$recaptchaSubmitButton.attr("data-callback", this.invisibleRecaptchaId);
+			}
+			else {
+				// Submit form via AJAX after click on submit button
+				var self = this;
+				this.$submitButton.click(function(e) {
+					e.preventDefault();
+					self.submitForm();
 				});
-			});
+			}
 		}
 
 		// For each form here activate ajax request
